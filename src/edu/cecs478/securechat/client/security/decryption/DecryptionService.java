2b3bb5e0979e98a1075b4dd1a30f74f1d51ed023
SaschaBergmann <saschamarc.bergmann@student.csulb.edu>
2017-10-26 14:06:04 -0700

    Added comments

--- src/edu/cecs478/securechat/client/security/decryption/DecryptionService.java
+++ src/edu/cecs478/securechat/client/security/decryption/DecryptionService.java
@@ -29,12 +29,22 @@ import java.util.Arrays;
  */
 public class DecryptionService{
 
+    /**
+     * Decrypts a given message with the parameters given in the message object
+     * First the keys are decrypted with a given RSA Key File, then the hash will be checked. Afterwards the message gets decrypted
+     * @param msg message object containing the ciphertext, keys, iv and hash
+     * @param rsaKeyFilePath Path to the private key file for decryption
+     * @return returns the parameter Message object with all decrypted values
+     * @throws IntegrityNotGuaranteedException Is thrown if the Hash calculated does not match the one in the object
+     */
     public static Message decrypt(Message msg, String rsaKeyFilePath) throws IntegrityNotGuaranteedException{
+
         byte[] keyString = RSADecrypt(msg.getKeys(), rsaKeyFilePath);
 
         byte[] aesKey = ArrayUtils.subarray(keyString, 0, 32);
         byte[] HMACKey = ArrayUtils.subarray(keyString, 32, 64);
 
+        //Check Hashes and if they are not the same throw an exception
         if (!Arrays.equals(HMACService.CreateHMACHash(msg.getMessage(),HMACKey),(msg.getHMACHash()))) throw new IntegrityNotGuaranteedException(msg);
 
         try {
@@ -48,6 +58,14 @@ public class DecryptionService{
         return msg;
     }
 
+    /**
+     * Perform a AES Decryption on the content with the given key and iv
+     * @param content bytes that should be decrypted
+     * @param aesKey AES Key used for decryption
+     * @param iv IV that should be used in decryption
+     * @return the bytes after decryption
+     * @throws Exception Every exception thrown in this method not be caught but thrown further
+     */
     private static byte[] AESDecrypt(byte[] content, byte[] aesKey, IvParameterSpec iv) throws Exception {
         PaddedBufferedBlockCipher aes = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()), new PKCS7Padding());
         CipherParameters ivAndKey = new ParametersWithIV(new KeyParameter(aesKey), iv.getIV());
@@ -55,24 +73,31 @@ public class DecryptionService{
         return SecurityHelper.cipherData(aes, content);
     }
 
-
+    /**
+     * Performs a RSA Decryption on the input bytes using the private key file. The cipher is RSA/NONE/OAEPWithSHA256AndMGF1Padding
+     * @param input input that should be decrypted
+     * @param rsaKeyFilePath Path to the private key file
+     * @return decrypted bytes
+     */
     private static byte[] RSADecrypt(byte[] input, String rsaKeyFilePath) {
         byte[] decryptedData = null;
         try {
+            //Adding bouncy castle as security provider
             Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
+            //Reading the Private Key using the SecureChat password finder
             PEMReader reader = new PEMReader(new FileReader(rsaKeyFilePath), new SecureChatPasswordFinder());
             try {
                 Object o = reader.readObject();
                 if (o instanceof KeyPair){
                     KeyPair kp = (KeyPair)o;
+                    //Setting cipher and decrypting mode
                     Cipher cipher = Cipher.getInstance("RSA/NONE/OAEPWithSHA256AndMGF1Padding", "BC");
-
                     cipher.init(Cipher.DECRYPT_MODE, kp.getPrivate());
 
+                    //Decrypting the data
                     decryptedData = cipher.doFinal(input);
                 }
-
             } catch (Exception e){
                 e.printStackTrace();
             }
--- src/edu/cecs478/securechat/client/security/encryption/EncryptionService.java
+++ src/edu/cecs478/securechat/client/security/encryption/EncryptionService.java
@@ -1,8 +1,8 @@
 package edu.cecs478.securechat.client.security.encryption;
 
-import com.sun.deploy.util.ArrayUtil;
 import edu.cecs478.securechat.client.model.Message;
 import edu.cecs478.securechat.client.security.SecurityHelper;
+import edu.cecs478.securechat.client.security.exceptions.IntegrityNotGuaranteedException;
 import edu.cecs478.securechat.client.security.integrity.HMACService;
 import org.apache.commons.lang3.ArrayUtils;
 import org.bouncycastle.crypto.CipherParameters;
@@ -16,15 +16,32 @@ import org.bouncycastle.openssl.PEMReader;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.FileReader;
-import java.security.*;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Security;
 
 /**
  * Created by sasch on 09/10/2017.
  */
 public class EncryptionService {
-
+    /**
+     * Decrypts a given message with the parameters given in the message object
+     * First the keys are decrypted with a given RSA Key File, then the hash will be checked. Afterwards the message gets decrypted
+     * @param msg message object containing the ciphertext, keys, iv and hash
+     * @param rsaKeyFilePath Path to the private key file for decryption
+     * @return returns the parameter Message object with all decrypted values
+     * @throws IntegrityNotGuaranteedException Is thrown if the Hash calculated does not match the one in the object
+     */
+    /**
+     * Encrypts a given message
+     * First a key is generated for AES encryption on the message in the given object. Then a hash is created using HMAC.
+     * the keys are then concatenated and encrypted using RSA and the given public key
+     * @param msg message object containing the message and memebers to put the encryption parameters
+     * @param rsaKeyFilePath Path to the public key file for the RSA encryption
+     * @return message parameter object now containing the ciphertext, keys, iv
+     */
     public static Message encrypt(Message msg, String rsaKeyFilePath){
         try {
             byte[] aESKey = EncryptionService.createPseudoRandomKey(256/8);
@@ -35,7 +52,7 @@ public class EncryptionService {
             msg.setHMACHash(HMACService.CreateHMACHash(msg.getMessage(),hmacKey));
 
             byte[] concatenated = ArrayUtils.addAll(aESKey,hmacKey);
-            //msg.setKeys(concatenated);
+
             msg.setKeys(encryptRSA(concatenated, rsaKeyFilePath));
             return msg;
         }catch (Exception e){
@@ -44,19 +61,28 @@ public class EncryptionService {
         }
     }
 
+    /**
+     * Encrypts the byte input with the public key
+     * @param input input to be encrypted
+     * @param rsaKeyFilePath File path to the public key file
+     * @return encrypted data
+     */
     private static byte[] encryptRSA(byte[] input, String rsaKeyFilePath){
         byte[] encryptedData = null;
         try {
+            //Setting bouncy castle as security provider
             Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
+            //Reading the public key file
             PEMReader reader = new PEMReader(new FileReader(rsaKeyFilePath));
             try {
                 Object o = reader.readObject();
                 if (o instanceof PublicKey){
                     PublicKey pubk = (PublicKey)o;
+
+                    //Setting the cipher suite
                     Cipher cipher = Cipher.getInstance("RSA/NONE/OAEPWithSHA256AndMGF1Padding","BC");
-
-
+                    //Setting the cipher to decryption
                     cipher.init(Cipher.ENCRYPT_MODE, pubk);
                     encryptedData = cipher.doFinal(input);
                 }
@@ -71,6 +97,14 @@ public class EncryptionService {
         return encryptedData;
     }
 
+    /**
+     * Perform a AES Enryption on the content with the given key and iv
+     * @param content content to be encrypted
+     * @param aesKey The aes key that is used
+     * @param iv IV that is used in the encryption
+     * @return encrypted bytes
+     * @throws Exception Every exception thrown in this method not be caught but thrown further
+     */
     private static byte[] AESEncrypt(byte[] content, byte[] aesKey, IvParameterSpec iv) throws Exception {
         PaddedBufferedBlockCipher aes = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()), new PKCS7Padding());
         CipherParameters ivAndKey = new ParametersWithIV(new KeyParameter(aesKey), iv.getIV());
@@ -78,6 +112,12 @@ public class EncryptionService {
         return SecurityHelper.cipherData(aes, content);
     }
 
+    /**
+     * Creates a random key using the PRNG/Entry pool
+     * @param cipherBlockSize Size of the key
+     * @return Randomly generated Key
+     * @throws NoSuchAlgorithmException If the random algorithm isn't found this exception is thrown
+     */
     private static byte[] createPseudoRandomKey(int cipherBlockSize) throws NoSuchAlgorithmException {
         SecureRandom randomSecureRandom = SecureRandom.getInstance("SHA1PRNG");
         byte[] iv = new byte[cipherBlockSize];
